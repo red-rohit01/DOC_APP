44c755d1d913563894bd2544203b1c5f5e261754
red-rohit01 <rr42041@gmail.com>
2023-06-22 18:41:45 +0530

    build added

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const colors = require("colors");
+const moragan = require("morgan");
+const dotenv = require("dotenv");
+const mongoose=require("mongoose");
+const path=require("path");
+//dotenv conig
+dotenv.config();
+
+//mongodb connection
+const dblink=process.env.MONGODB_URL;
+const connectDB = async () => {
+    try {
+      await mongoose.connect(dblink);
+      console.log(`Mongodb connected`);
+    } catch (error) {
+      console.log(`Mongodb Server Issue ${error}`);
+    }
+  };
+  connectDB();
+
+//rest obejct
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(moragan("dev"));
+
+//routes
+app.use("/api/v1/user", require("./routes/userRoutes"));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
+app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
+
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(
+    `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
+      .bgCyan.white
+  );
+});
